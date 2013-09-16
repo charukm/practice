3a512f06e30fe8654d8715ee5b9262c1d17a4b98
unknown <5202@SyedJuned.Hirepro.local>
2013-09-16 18:36:56 +0530

    Syed enter new Label

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,7 +6,7 @@ import play.api.mvc._
 object Application extends Controller {
   
   def index = Action {
-    Ok(views.html.index("New play framework project, thn, with commented by prateek changes in master"))
+    Ok(views.html.index("framework project, thn, with commented by prateek changes in master"))
   }
   
 }
